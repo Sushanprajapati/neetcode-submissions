a2ba8b24f7de871ddfa59c90f2c675d5382b8624
Sushan Prajapati <37505317+Sushanprajapati@users.noreply.github.com>
2026-08-03 13:27:06 -0700

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+    // We'll try to implement level order traversal 
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        Queue<TreeNode> q = new LinkedList<>();
+        StringBuilder st = new StringBuilder();
+
+        q.offer(root);
+
+        while(!q.isEmpty()) {
+            TreeNode node = q.poll(); 
+
+            if (node == null) {
+                st.append("N ");
+                continue;
+            }
+            st.append(node.val + " ");
+            q.offer(node.left);
+            q.offer(node.right);        
+        }
+        return st.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == "") return null;
+        Queue<TreeNode> q = new LinkedList<>();
+        String[] arr = data.split(" ");
+        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+        q.offer(root);
+
+        for(int i = 1; i < arr.length; i++) {
+            TreeNode node = q.poll();
+
+            if (!arr[i].equals("N")) {
+                TreeNode left = new TreeNode(Integer.parseInt(arr[i]));
+                node.left = left;
+                q.offer(left);
+            }
+            if (!arr[++i].equals("N")) {
+                TreeNode right = new TreeNode(Integer.parseInt(arr[i]));
+                node.right = right;
+                q.offer(right);
+            }
+        }
+        return root;
+    }
+}
